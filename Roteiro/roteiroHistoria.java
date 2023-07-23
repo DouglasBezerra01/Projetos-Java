1a7e108cbd5f04d8bf8f299b5fd54058314b9627
DouglasBezerra01 <137025820+DouglasBezerra01@users.noreply.github.com>
2023-07-22 23:38:34 -0300

    Update roteiroHistoria.java

--- Roteiro/roteiroHistoria.java
+++ Roteiro/roteiroHistoria.java
@@ -52,14 +52,14 @@ public class roteiroHistoria {
                                     + "como o protetor do reino e vive o resto de seus dias como um símbolo de esperança e coragem.";
 
         Capitulo capG022 = new Capitulo(leitor, jogador);
-        capG021.roteiro = "\nO guerreiro luta valentemente contra as forças do mal, mas é derrotado na batalha final.\n"
+        capG022.roteiro = "\nO guerreiro luta valentemente contra as forças do mal, mas é derrotado na batalha final.\n"
                                     + "Ele sacrifica sua própria vida para proteger Eldoria, deixando um legado de coragem e inspiração\n"
                                     + "para as gerações futuras. Embora seu corpo tenha perecido, seu espírito permanece vivo nos\n"
                                     + "corações do povo de Eldoria, que se unem para honrar sua memória e continuar a luta contra o mal.";
         
 
-            } else { // História 2 do guerreiro
-                Capitulo capG03 = new Capitulo(2);
+            //} else { // História 2 do guerreiro
+                Capitulo capG03 = new Capitulo(leitor, jogador);
                 capG03.roteiro = "\nDepois de meses de busca, " + jogador.getNome()
                         + " finalmente chegou ao templo sagrado escondido nas montanhas.\n"
                         + "Lá, ele foi recebido pelos monges guardiões do templo, que reconheceram sua coragem e determinação.\n"
@@ -69,17 +69,17 @@ public class roteiroHistoria {
                         + "Ao empunhar a Espada da Luz, sentiu um poder divino fluir através de seu corpo, fortalecendo sua determinação em proteger Eldoria.\n"
                         + "Agora, equipado com a espada lendária, ele está pronto para enfrentar qualquer desafio que surja em seu caminho.\n";
 
-                capG03.escolhas[0] = "Enfrentar os inimigos menores primeiro\n";
-                capG03.escolhas[1] = "Lutar de frente com o líder maligno\n";
-                capG03.mostrar(jogador);
+                //capG03.escolhas[0] = "Enfrentar os inimigos menores primeiro\n";
+                //capG03.escolhas[1] = "Lutar de frente com o líder maligno\n";
+                //capG03.mostrar(jogador);
 
-                if (capG03.escolha(leitor) == 0) { // Final 2.1 guerreiro
+                //if (capG03.escolha(leitor) == 0) { // Final 2.1 guerreiro
                     System.out.println(
                             "\nAo enfrentar inúmeras adversidades, o guerreiro finalmente encontra o líder das forças malignas\n"
                                     + "e o desafia em um duelo decisivo. Com sua habilidade e determinação, ele derrota o vilão e\n"
                                     + "destrói o mal que assolava Eldoria. O guerreiro retorna como um herói aclamado,\n"
                                     + "recebendo honrarias e gratidão de todo o reino.");
-                } else { // Final 2.2 guerreiro
+                //} else { // Final 2.2 guerreiro
                     System.out.println(
                             "\nO guerreiro enfrenta o líder das forças malignas em uma batalha épica, mas suas forças são superadas.\n"
                                     + "Ele é derrotado e capturado pelo vilão, que usa sua energia para alimentar sua maldade.\n"
@@ -88,8 +88,8 @@ public class roteiroHistoria {
                 }
             }
 
-        } else { // História da feiticeira
-            Capitulo capF01 = new Capitulo(2);
+        //} else { // História da feiticeira
+            Capitulo capF01 = new Capitulo(leitor, jogador);
             capF01.roteiro = "\nA feiticeira " + jogador.getNome()
                     + " possuía uma conexão profunda com as forças mágicas do reino.\n"
                     + "Seus poderes eram vastos e ela era respeitada por seu conhecimento e habilidades.\n"
@@ -101,8 +101,8 @@ public class roteiroHistoria {
             capF01.escolhas[1] = "Usar leituras de visão\n";
             capF01.mostrar(jogador);
 
-            if (capF01.escolha(leitor) == 0) { // História 1 da feiticeira
-                Capitulo capF02 = new Capitulo(2);
+            //if (capF01.escolha(leitor) == 0) { // História 1 da feiticeira
+                Capitulo capF02 = new Capitulo(leitor, jogador);
                 capF02.roteiro = "\nApós uma intensa pesquisa nos antigos registros mágicos, " + jogador.getNome()
                         + " descobriu que o Cetro da Sabedoria\n"
                         + "estava guardado em uma torre encantada no coração da Floresta Proibida. Armada com seus conhecimentos e feitiços,\n"
@@ -113,24 +113,24 @@ public class roteiroHistoria {
                         + " sentiu um fluxo de energia mágica percorrer seu corpo, capacitando-a com um poder além de sua imaginação.\n"
                         + "Agora, com o Cetro da Sabedoria em mãos, ela está pronta para enfrentar as trevas e proteger Eldoria.\n";
 
-                capF02.escolhas[0] = "Lutar diretamente com o vilão no covil\n";
-                capF02.escolhas[1] = "Esgueirar-se pelo covil para emboscar o vilão\n";
-                capF02.mostrar(jogador);
+                //capF02.escolhas[0] = "Lutar diretamente com o vilão no covil\n";
+                //capF02.escolhas[1] = "Esgueirar-se pelo covil para emboscar o vilão\n";
+                //capF02.mostrar(jogador);
 
-                if (capF02.escolha(leitor) == 0) { // Final 1.1 feiticeira
+                //if (capF02.escolha(leitor) == 0) { // Final 1.1 feiticeira
                     System.out.println(
                             "\nA feiticeira usa sua magia poderosa e conhecimento místico para superar os desafios e encontrar o caminho até o covil do mal.\n"
                                     + "Lá, ela enfrenta o vilão em um duelo de poderes. Com sua sabedoria e habilidades, ela consegue derrotá-lo e selar sua existência para sempre.\n"
                                     + "A feiticeira retorna a Eldoria como a protetora do reino, usando seus poderes para trazer prosperidade e harmonia.");
-                } else { // Final 1.2 feiticeira
+                //} else { // Final 1.2 feiticeira
                     System.out.println(
                             "\nAo se aventurar pelo covil do mal, a feiticeira é enfeitiçada por uma armadilha astuta. Seus poderes são aprisionados,\n"
                                     + "ela se torna impotente diante do vilão. Eldoria cai nas mãos do mal e a feiticeira, sem seus poderes, é condenada a viver\n"
                                     + "uma vida de desespero e arrependimento. Seu destino serve como um lembrete trágico dos perigos da ambi\u00E7\u00E3o desmedida.");
                 }
 
-            } else { // História 2 da feiticeira
-                Capitulo capF03 = new Capitulo(2);
+            //} else { // História 2 da feiticeira
+                Capitulo capF03 = new Capitulo(leitor, jogador);
                 capF03.roteiro = "\nGuiada por visões místicas, " + jogador.getNome()
                         + " embarcou em uma jornada até as Montanhas Celestiais,\n"
                         + "um lugar sagrado onde o conhecimento ancestral estava preservado. Lá, ela enfrentou os desafios do labirinto mágico,\n"
@@ -142,17 +142,17 @@ public class roteiroHistoria {
                         + " sentiu uma conexão profunda com o cosmos, seus poderes se ampliaram e sua determinação foi reforçada.\n"
                         + "Agora, com o Cetro da Sabedoria em mãos, ela está pronta para enfrentar qualquer desafio e proteger Eldoria com sua magia.\n";
 
-                capF03.escolhas[0] = "Lutar diretamente com o vilão no covil\n";
-                capF03.escolhas[1] = "Esgueirar-se pelo covil para emboscar o vilão\n";
-                capF03.mostrar(jogador);
+                //capF03.escolhas[0] = "Lutar diretamente com o vilão no covil\n";
+                //capF03.escolhas[1] = "Esgueirar-se pelo covil para emboscar o vilão\n";
+                //capF03.mostrar(jogador);
 
-                if (capF03.escolha(leitor) == 0) {
+                //if (capF03.escolha(leitor) == 0) {
                     System.out.println(
                             "\nA feiticeira, guiada por visões cósmicas, se torna uma aprendiz dos mestres da magia nas Montanhas Celestiais.\n"
                                     + "Ela aprimora seus poderes e recebe ensinamentos ancestrais. Com o Cetro da Sabedoria como sua ferramenta,\n"
                                     + "ela retorna a Eldoria e usa sua magia para eliminar as forças do mal. O reino floresce sob sua proteção,\n"
                                     + "enquanto ela continua sua jornada como a guardiã da magia.");
-                } else {
+                //} else {
                     System.out.println(
                             "\nA feiticeira embarca em uma jornada perigosa para dominar seu novo poder com o Cetro da Sabedoria.\n"
                                     + "No entanto, a busca pelo conhecimento proibido a consome, e ela se deixa levar pela escuridão.\n"
